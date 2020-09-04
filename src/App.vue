58ed99c86289002ec9cfdfaa6314098f87407dbc
Luca Calcaterra <calcaterra.luca@gmail.com>
2020-09-04 18:40:51 +0200

    clean

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <v-app id="inspire">
+    <v-app-bar app color="teal" dark>
+      <v-toolbar-title>Monitoraggio Covid 19</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container class fluid>
+        <v-row align="center" justify="center">
+          <v-col class="text-center" cols="4">
+            <v-card class="mx-auto">
+              <span>Provincia</span>
+            </v-card>
+          </v-col>
+          <v-col cols="8">
+            <v-card class="mx-auto pa-5">
+              <p class="font-weight-black">Provincia</p>
+              <line-chart
+                :chartData="arrPositivi"
+                :options="chartOptions"
+                :chartColors="positiveChartColors"
+                label="Positivi"
+              />
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <v-footer color="teal" app>
+      <v-spacer></v-spacer>
+
+      <span
+        class="white--text"
+      >&copy; Ufficio Informatica - Prefettura di Macerata - {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import moment from "moment";
+
+import LineChart from "./components/LineChart";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    LineChart,
+  },
+  /* name: "Dashboard",
+  props: {
+    source: String,
+  }, */
+  data: () => ({
+    drawer: null,
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    arrPositivi: [],
+    positiveChartColors: {
+      borderColor: "#077187",
+      pointBorderColor: "#0E1428",
+      pointBackgroundColor: "#AFD6AC",
+      //backgroundColor: "#74A57F"
+    },
+    chartOptions: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  }),
+
+  async created() {
+    const { data } = await axios.get(
+      "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json"
+    );
+    //const {data} = await axios.get('/db.json')
+    //console.log(data);
+
+    let dataMc = data.filter((elem) => elem.sigla_provincia === "MC");
+    dataMc.forEach((d) => {
+      const date = moment(d.data, "YYYYMMDD").format("DD/MM");
+      const {
+        totale_casi,
+        //totpositivi,
+      } = d;
+
+      // this.arrTotali.push({ date, total: totale_casi });
+      this.arrPositivi.push({ date, total: totale_casi });
+    });
+  },
+};
+</script>
